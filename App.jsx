20baca40db3d32822ce677bdb770ec556407eb0d
Sumit Patel <sumitz1207@gmail.com>
2023-03-04 22:37:38 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Map from "Map/Map.jsx";
+
+
+//central React App component
+//random tailwind adds to test functionality
+const App = () => {
+  return (
+      <div className="max-w-sm rounded-shadow border-4">testing</div>
+      <MapView />
+  );
+};
+
+export default App;
